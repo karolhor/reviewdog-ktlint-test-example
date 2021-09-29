1b4aa4fafc6e6186f14edb25535c4127ca6794f7
Karol Horowski <karol.horowski@allegro.pl>
2021-09-29 12:36:49 +0200

    example code

--- src/main/kotlin/com/github/karolhor/reviedogexample/main.kt
+++ src/main/kotlin/com/github/karolhor/reviedogexample/main.kt
@@ -1,11 +1,12 @@
 package com.github.karolhor.reviedogexample
 
-// import java.util.*
-// import com.github.karolhor.reviedogexample.other.Compute
+import com.github.karolhor.reviedogexample.other.Compute
+import java.util.UUID
 
 fun main() {
+    val id = UUID.randomUUID()
 //    val id = UUID.randomUUID().let {it.toString() }
-//    val sum = Compute.add(5, 3)
+    val sum = Compute.add(5, 3)
 //    println("Example msg: $id")
-//    println("Result: $sum")
+    println("Result: $sum")
 }
